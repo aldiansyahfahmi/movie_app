2ff5caa9702d18c91219f365962c5725f9268560
aldiansyahfahmi <aldiansyahfahmi6@gmail.com>
2023-07-28 13:58:41 +0800

    add colors

---
+++ lib/shared_libraries/resources/colors.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #0D0C27
+  static const Color black = Color(0xFF0D0C27);
+
+  /// Color: #252538
+  static const Color gray = Color(0xFF252538);
+
+  /// Color: #F09631
+  static const Color orange = Color(0xFFF09631);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+}
